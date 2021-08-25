f8f1bfc3e57e6d7ffd943dda1603c218a034972f
gothae <koxogml@naver.com>
2021-08-25 17:39:21 +0900

    0825

---
+++ src/components/header.js
@@ -0,0 +1,11 @@
+export default function Header(){
+    return(
+        <>
+            <span>airbnb</span>
+            <span>숙소</span>
+            <span>체험</span>
+            <span>온라인 체험</span>
+            <span>호스트 되기</span>
+        </>
+    );
+}
