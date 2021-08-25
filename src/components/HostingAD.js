93701a3a95b82d13f55ba1e62392741dc204ce2d
uiseop <bono521@naver.com>
2021-08-26 00:33:07 +0900

    사이즈 조정

--- src/components/HostingAD.js
+++ src/components/HostingAD.js
@@ -7,15 +7,15 @@ export default function HostingAD() {
         src={datas.hosting}
         title="hello"
       />
-      <p className="absolute top-48 left-20 text-5xl font-bold text-white">
+      <p className="absolute top-44 left-20 text-5xl font-bold text-white">
         호스팅 시작하기
       </p>
-      <p className="absolute top-64 left-20 text-xl text-white">
+      <p className="absolute top-60 left-20 text-xl text-white">
         숙소를 공유하여 부수입을 올리고
         <br />
         새로운 가능성을 만나세요.
       </p>
-      <p className="absolute bottom-32 left-20 border py-2 px-10 rounded-lg bg-white font-bold text-black">자세히 알아보기</p>
+      <p className="absolute bottom-36 left-20 border py-3 px-8 rounded-lg bg-white font-bold text-black">자세히 알아보기</p>
     </div>
   );
 }
