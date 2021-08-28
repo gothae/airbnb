96ad64675b744b16c6a5f5b565ca71a5732c255e
gothae <koxogml@naver.com>
2021-08-28 22:03:51 +0900

    db

--- src/pages/experiences_online.js
+++ src/pages/experiences_online.js
@@ -3,7 +3,7 @@ import React from "react";
 
 export default function ExperiencesOnline(){
     useEffect(() =>{
-        document.title = 'experiences_online';
+        document.title = 'Airbnb';
     },[]);
 
     return (
